2b0728f4b4618e5a2c10fc0916e68cbd8e68af7e
CKloesch <90834327+CKloesch@users.noreply.github.com>
2021-11-27 12:19:21 +0100

    upload WebGL Stand letzte Stunde

--- docs/Build/docs.framework.js
+++ docs/Build/docs.framework.js
@@ -1297,7 +1297,7 @@ function _emscripten_asm_const_ii(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 2303136;
+STATICTOP = STATIC_BASE + 2303280;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -3351,7 +3351,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 2303136;
+var STATIC_BUMP = 2303280;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
